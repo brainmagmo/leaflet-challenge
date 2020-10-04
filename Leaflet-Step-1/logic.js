43b8949b399ac551b8e246ee58bd972042259557
Trevor Kleinstuber <trevor.kleinstuber@gmail.com>
2020-10-03 23:14:56 -0700

    hrm

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -1,33 +1,16 @@
 // Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
-  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
-
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
   // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data.features);
+  createMap(data.features);
 });
 
-function createFeatures(earthquakeData) {
-
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
-  function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-  }
-
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
-
-  // Sending our earthquakes layer to the createMap function
-  createMap(earthquakes);
+function color_f (depth, range) {
+  return "black";
 }
 
-function createMap(earthquakes) {
+function createMap(eqData) {
 
   // Define streetmap and darkmap layers
   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -53,23 +36,33 @@ function createMap(earthquakes) {
   };
 
   // Create overlay object to hold our overlay layer
-  var overlayMaps = {
-    Earthquakes: earthquakes
-  };
 
   // Create our map, giving it the streetmap and earthquakes layers to display on load
   var myMap = L.map("map", {
     center: [
-      37.09, -95.71
+      32.7157, -117.1611
     ],
-    zoom: 5,
-    layers: [streetmap, earthquakes]
+    zoom: 10,
+    layers: [streetmap, eqData]
   });
 
   // Create a layer control
   // Pass in our baseMaps and overlayMaps
   // Add the layer control to the map
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
+
+  eqData.forEach(eq => {
+    var mag = eq.properties.mag;
+    var lat = eq.geometry.coordinates[1];
+    var lng = eq.geometry.coordinates[0];
+    //var depth = eq.geometry.coordinates[2];
+
+      L.circle([lat,lng], {
+        
+        color: "white",
+        fillColor: "black",
+        radius: mag * 10
+    }).addTo(myMap);
+  });
+
+  eqData.addTo(myMap);
 }
